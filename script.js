c2e149005cbbc47ccd262ac076c9fa1ca45b67e9
Sam Bowden-Williams <SBowdenWilliams@spartaglobal.com>
2019-01-16 16:22:35 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//Target table attributes - data nums
+//store data nums in array - possibly td tags
+//can add and delete classes from element - research
+//sparta-tictactoe
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    
+
+
+
+
+
+
+})
